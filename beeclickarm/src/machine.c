8df704f6eeaa83219bb3aea6ecade62ab4b39f98
Tomas Bures <bures@d3s.mff.cuni.cz>
2013-08-23 19:53:08 +0200

    Some minor things in beeclickarm. Initial commit of the Java binding.

--- beeclickarm/src/machine.c
+++ beeclickarm/src/machine.c
@@ -1,12 +1,13 @@
 #include "machine.h"
 #include <stm32f4_discovery.h>
 #include <string.h>
+#include <stdio.h>
 #include "uart_io.h"
 
 
-#define LED_RXTX LED3
-#define LED_OUT_OF_SYNC LED4
-#define LED_RF_RECV LED5
+#define LED_RXTX LED4
+#define LED_OUT_OF_SYNC LED5
+#define LED_RF_RECV LED3
 #define LED_RF_SEND LED6
 
 
@@ -17,12 +18,11 @@
 #define TOHD_SYNC_PATTERN "Hello there!"
 #define TOHD_SYNC_PATTERN_LENGTH 12
 
-#define VOLUNATARY_SYNC_PERIOD 5000 // Every nth period, we sent SYNC just by ourselves
+#define VOLUNATARY_SYNC_PERIOD 1000000 // Every nth period, we sent SYNC just by ourselves
+#define MAX_INFO_TEXT_LENGTH 255
 
 typedef enum {
-	TOD_SYNC_REQUESTED,
 	TOD_SYNC,
-	TOD_SYNC_REQUEST_NEEDED,
 	TOD_SEND_PACKET,
 	TOD_SET_CHANNEL,
 	TOD_TYPES_NUMBER
@@ -35,9 +35,9 @@ typedef struct {
 
 typedef struct {
 	TODType type;
+	uint8_t length;
 	uint32_t seq;
 	uint8_t data[MAX_RF_PACKET_LENGTH];
-	uint8_t length;
 } TODSendPacket;
 
 typedef struct {
@@ -47,41 +47,61 @@ typedef struct {
 
 typedef union {
 	TODType type;
-	TODSync init;
+	TODSync sync;
 	TODSendPacket sendPacket;
 	TODSetChannel setChannel;
 } TOD;
 
-uint8_t TODMsgLengths[TOD_TYPES_NUMBER] = {
-	sizeof(TODType), // TOD_SYNC_REQUESTED
+
+size_t todFixedLengths[TOD_TYPES_NUMBER] = {
 	sizeof(TODSync),
-	sizeof(TODType), // TOD_SYNC_REQUEST_NEEDED
-	sizeof(TODSendPacket),
+	0,
 	sizeof(TODSetChannel)
 };
 
+static size_t todLengthHandlerFixed(TOD msg) {
+	size_t length = todFixedLengths[msg.type];
+
+	assert_param(length != 0);
+
+	return length;
+}
+
+static size_t todLengthHandlerSendPacket(TOD msg) {
+	return sizeof(TODType) + sizeof(uint8_t) + sizeof(uint32_t) + msg.sendPacket.length;
+}
+
+typedef size_t(*TODLengthHandler)(TOD msg);
+
+TODLengthHandler todLengthHandlers[TOD_TYPES_NUMBER] = {
+		todLengthHandlerFixed,		// TOD_SYNC
+		todLengthHandlerSendPacket,	// TOD_SEND_PACKET
+		todLengthHandlerFixed		// TOD_SET_CHANNEL
+};
+
+
 
 typedef enum {
-	TOH_SYNC_REQUESTED,
 	TOH_SYNC,
 	TOH_RECV_PACKET,
 	TOH_PACKET_SENT,
 	TOH_CHANNEL_SET,
+	TOH_INFO,
 	TOH_TYPES_NUMBER
 } TOHType;
 
 typedef struct {
 	TOHType type;
 	uint8_t pattern[TOHD_SYNC_PATTERN_LENGTH];
-} TOHInit;
+} TOHSync;
 
 typedef struct {
 	TOHType type;
-	uint8_t data[MAX_RF_PACKET_LENGTH];
 	uint8_t length;
 	uint8_t rssi;
 	uint8_t lqi;
 	uint8_t fcs;
+	uint8_t data[MAX_RF_PACKET_LENGTH];
 } TOHRecvPacket;
 
 typedef struct {
@@ -94,21 +114,56 @@ typedef struct {
 	uint8_t channel;
 } TOHChannelSet;
 
+typedef struct {
+	TOHType type;
+	uint8_t length;
+	char text[MAX_INFO_TEXT_LENGTH];
+} TOHInfo;
+
 typedef union {
 	TOHType type;
-	TOHInit init;
+	TOHSync sync;
 	TOHRecvPacket recvPacket;
 	TOHPacketSent packetSent;
 	TOHChannelSet channelSet;
+	TOHInfo info;
 } TOH;
 
-uint8_t TOHMsgLengths[TOH_TYPES_NUMBER] = {
-	sizeof(TOHType), // TOD_INIT_REQUESTED
-	sizeof(TOHInit),
-	sizeof(TOHRecvPacket),
+
+size_t tohFixedLengths[TOH_TYPES_NUMBER] = {
+	sizeof(TOHSync),
+	0,
 	sizeof(TOHPacketSent),
-	sizeof(TOHChannelSet)
+	sizeof(TOHChannelSet),
+	0
 };
+
+static size_t tohLengthHandlerFixed(TOH msg) {
+	size_t length = tohFixedLengths[msg.type];
+
+	assert_param(length != 0);
+
+	return length;
+}
+
+static size_t tohLengthHandlerRecvPacket(TOH msg) {
+	return sizeof(TOHType) + sizeof(uint8_t) * 4 + msg.recvPacket.length;
+}
+
+static size_t tohLengthHandlerInfo(TOH msg) {
+	return sizeof(TOHType) + sizeof(uint8_t) + msg.info.length;
+}
+
+typedef size_t(*TOHLengthHandler)(TOH msg);
+
+TOHLengthHandler tohLengthHandlers[TOH_TYPES_NUMBER] = {
+		tohLengthHandlerFixed,		// TOH_SYNC
+		tohLengthHandlerRecvPacket,	// TOH_RECV_PACKET
+		tohLengthHandlerFixed,		// TOH_PACKET_SENT
+		tohLengthHandlerFixed,		// TOH_CHANNEL_SET
+		tohLengthHandlerInfo		// TOH_INFO
+};
+
 
 
 TOD todQueue[MAX_TOD_QUEUE_LENGTH];
@@ -159,7 +214,7 @@ static void incTOHQueueWritePtr() {
 uint32_t rxIdleCount, txIdleCount;
 
 static void updateRXTXLed() {
-	if (rxIdleCount > 10 && txIdleCount > 10) {
+	if (rxIdleCount > 100 && txIdleCount > 100) {
 		STM_EVAL_LEDOff(LED_RXTX);
 	} else {
 		STM_EVAL_LEDOn(LED_RXTX);
@@ -174,27 +229,24 @@ enum {
 
 uint32_t rxBufferPtr = 0;
 
-TODSync correctTODInitMsg = {
+TODSync correctTODSyncMsg = {
 	.type = TOD_SYNC,
 	.pattern = TOHD_SYNC_PATTERN
 };
-
-static void rxPanic() {
-	rxBufferPtr = 0;
-	rxState = RX_SYNC;
-
-	todQueue[todQueueWritePtr].type = TOD_SYNC_REQUEST_NEEDED;
-	incTODQueueWritePtr();
-
-	STM_EVAL_LEDOn(LED_OUT_OF_SYNC);
-}
 
 static void handleRX() {
 	if (uart2IsBreakOrError()) {
 		uart2ClearBreakOrError();
 		uart2ClearBreakOrError();
 
-		rxPanic();
+		rxBufferPtr = 0;
+		rxState = RX_SYNC;
+	}
+
+	if (rxState == RX_SYNC) {
+		STM_EVAL_LEDOn(LED_OUT_OF_SYNC);
+	} else {
+		STM_EVAL_LEDOff(LED_OUT_OF_SYNC);
 	}
 
 	if (uart2CanRecv()) {
@@ -202,18 +254,14 @@ static void handleRX() {
 		rxBuffer[rxBufferPtr++] = uart2RecvChar();
 
 		if (rxState == RX_SYNC) {
-
-			if (rxBuffer[rxBufferPtr - 1] == ((uint8_t*)&correctTODInitMsg)[rxBufferPtr - 1]) {
+			if (rxBuffer[rxBufferPtr - 1] == ((uint8_t*)&correctTODSyncMsg)[rxBufferPtr - 1]) {
 				if (rxBufferPtr == sizeof(TODSync)) {
 					rxBufferPtr = 0;
 					rxState = RX_OPERATIONAL;
-
-					STM_EVAL_LEDOff(LED_OUT_OF_SYNC);
 				}
 
 			} else {
 				rxBufferPtr = 0;
-				STM_EVAL_LEDOn(LED_OUT_OF_SYNC);
 			}
 
 		} else {
@@ -223,9 +271,14 @@ static void handleRX() {
 				rxState = RX_SYNC;
 
 			} else if (msgType < 0 || msgType >= TOD_TYPES_NUMBER) {
-				rxPanic();
+				rxBufferPtr = 0;
+				rxState = RX_SYNC;
 
-			} else if (rxBufferPtr == TODMsgLengths[msgType]) {
+			} else if (rxBufferPtr == todLengthHandlers[msgType](todQueue[todQueueWritePtr])) {
+				// Note that we call above a handler while the message is still only partially received. That is essentially wrong, but we assume the handlers
+				// won't return size which is smaller than the preamble of the message containing the length of the the message data. Once we read the length, the
+				// handler returns a correct value. Essentially it means that messages have to store the data length at the beginning and the length should be unsigned.
+
 				incTODQueueWritePtr();
 				rxBufferPtr = 0;
 			}
@@ -249,7 +302,7 @@ static void handleTX() {
 		TOHType msgType = tohQueue[tohQueueReadPtr].type;
 		assert_param(msgType >=0 && msgType < TOH_TYPES_NUMBER);
 
-		if (txBufferPtr == TOHMsgLengths[msgType]) {
+		if (txBufferPtr == tohLengthHandlers[msgType](tohQueue[tohQueueReadPtr])) {
 			incTOHQueueReadPtr();
 			txBufferPtr = 0;
 		}
@@ -265,21 +318,62 @@ static void handleTX() {
 
 
 uint32_t todHandlerProgress; // 0 means no handler is currently in progress
-uint32_t handleTODSyncCountdown = VOLUNATARY_SYNC_PERIOD - 1;
 
-static void handleTODSyncRequested() {
-	incTODQueueReadPtr();
+uint32_t lastInfoButtonState; // 0 - not pressed, 1 - pressed (action pending), 2 - pressed (action already performed), 3..max - waiting
+static void handleInfoButton() {
+	uint32_t infoButtonState = STM_EVAL_PBGetState(BUTTON_USER);
+
+	if (infoButtonState == 1 && lastInfoButtonState == 0) {
+		lastInfoButtonState = 1;
+	}
+
+	if (lastInfoButtonState == 1 && todHandlerProgress == 0) {
+		TOH *msg = &tohQueue[tohQueueWritePtr];
+
+		msg->type = TOH_INFO;
+
+		snprintf(msg->info.text, MAX_INFO_TEXT_LENGTH, "\n=== Info ===\nrxState: %d\n", rxState);
+		msg->info.length = strlen(msg->info.text);
+
+		incTOHQueueWritePtr();
+
+		lastInfoButtonState = 2;
+	}
+
+	if (infoButtonState == 0 && lastInfoButtonState == 2) {
+		lastInfoButtonState = 3;
+	}
+
+	if (lastInfoButtonState >= 3) {
+		if (lastInfoButtonState == 1000) {
+			lastInfoButtonState = 0;
+		} else {
+			lastInfoButtonState++;
+		}
+	}
 }
+
+uint32_t voluntarySyncCountdown = VOLUNATARY_SYNC_PERIOD - 1;
+TOHSync tohSyncMsg = {
+	.type = TOH_SYNC,
+	.pattern = TOHD_SYNC_PATTERN
+};
+static void handleVoluntarySync() {
+	if (voluntarySyncCountdown == 0) {
+		if (todHandlerProgress == 0) { // If we should already sync but there is a handler in progress, we simply wait by not decrementing the handleTODSyncCountdown
+			memcpy(&tohQueue[tohQueueWritePtr], &tohSyncMsg, sizeof(TOHSync));
+			incTOHQueueWritePtr();
+
+			voluntarySyncCountdown = VOLUNATARY_SYNC_PERIOD - 1;
+		}
+	} else {
+		voluntarySyncCountdown--;
+	}
+}
+
 
 static void handleTODSync() {
 	assert_param(0); // This is unimplemented method because sync is handler already in rx routine
-}
-
-static void handleTODSyncRequestNeeded() {
-	tohQueue[tohQueueWritePtr].type = TOH_SYNC_REQUESTED;
-	incTOHQueueWritePtr();
-
-	incTODQueueReadPtr();
 }
 
 static void handleTODSendPacket() {
@@ -293,9 +387,7 @@ static void handleTODSetChannel() {
 typedef void(*TODHandler)();
 
 TODHandler todHandlers[TOD_TYPES_NUMBER] = {
-	handleTODSyncRequested,
 	handleTODSync,
-	handleTODSyncRequestNeeded,
 	handleTODSendPacket,
 	handleTODSetChannel
 };
@@ -309,16 +401,8 @@ static void handleTOD() {
 		todHandlers[msgType]();
 	}
 
-	if (handleTODSyncCountdown == 0) {
-		if (todHandlerProgress == 0) { // If we should already sync but there is a handler in progress, we simply wait by not decrementing the handleTODSyncCountdown
-			tohQueue[tohQueueWritePtr].type = TOH_SYNC;
-			incTOHQueueWritePtr();
-
-			handleTODSyncCountdown = VOLUNATARY_SYNC_PERIOD - 1;
-		}
-	} else {
-		handleTODSyncCountdown--;
-	}
+	handleVoluntarySync();
+	handleInfoButton();
 }
 
 
@@ -328,6 +412,7 @@ void initSM() {
 	STM_EVAL_LEDInit(LED_RF_RECV);
 	STM_EVAL_LEDInit(LED_RF_SEND);
 
+	STM_EVAL_PBInit(BUTTON_USER, BUTTON_MODE_GPIO);
 	uart2Init();
 }
 
